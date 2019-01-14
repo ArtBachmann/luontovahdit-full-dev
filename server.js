2488350a338a2c007bfa5399a3929aff5fa5390d
art <bachmann.art@gmail.com>
2019-01-14 21:46:31 +0200

    prepared for deployment

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(compression())
+
 
 const db = require('./config/keys').mongoURI;
 
